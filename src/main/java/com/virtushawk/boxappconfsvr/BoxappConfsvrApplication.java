bde311620674845d749c89911aa7bb82c7a4cb6d
Raman Brukhanchyk <r.brukhanchyk@sap.com>
2024-08-23 21:31:09 +0400

    Initial commit

---
+++ src/main/java/com/virtushawk/boxappconfsvr/BoxappConfsvrApplication.java
@@ -0,0 +1,13 @@
+package com.virtushawk.boxappconfsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxappConfsvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxappConfsvrApplication.class, args);
+	}
+
+}
