66cef1e69c280109606f4ec7c0ca24bf34a3ccbd
Raman Brukhanchyk <r.brukhanchyk@sap.com>
2024-08-23 22:02:24 +0400

    commit

--- src/main/java/com/virtushawk/boxappconfsvr/BoxappConfsvrApplication.java
+++ src/main/java/com/virtushawk/boxappconfsvr/BoxappConfsvrApplication.java
@@ -2,12 +2,14 @@ package com.virtushawk.boxappconfsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BoxappConfsvrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoxappConfsvrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoxappConfsvrApplication.class, args);
+    }
 
 }
